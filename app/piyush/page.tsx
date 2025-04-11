e27ca55147988422304ce1f40ff0a4e0e18cb3f4
Nikhil <kumarnikhil48578@gmail.com>
2025-04-11 19:46:22 +0530

    fix

--- app/piyush/page.tsx
+++ app/piyush/page.tsx
@@ -1,14 +1,14 @@
 "use client"
 
 import React from "react"
-import InputArea from "../components/InputArea"
+import ChatArea from "../components/ChatArea"
 
 export default function PiyushPage() {
   const activeChatId = "piyush123"
 
   return (
     <main className="flex h-screen">
-      <InputArea activeChatId={activeChatId} />
+      <ChatArea activeChatId={activeChatId} />
     </main>
   )
 }
